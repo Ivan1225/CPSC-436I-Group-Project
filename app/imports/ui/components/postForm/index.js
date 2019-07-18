bd899c61d822cd4b937a2e8f68839fe92aa3c183
huuuuugo <huuuuugo222@outlook.com>
2019-07-18 09:37:12 -0700

    postform

--- app/imports/ui/components/postForm/index.js
+++ app/imports/ui/components/postForm/index.js
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Form, Row, Col, FormGroup, Button } from 'react-bootstrap';
-import {
-  withRouter
-} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { registerSuccessfullyHandler } from '../../actions/user';
 import StyledSignup from './styles';
 import { Accounts } from 'meteor/accounts-base';
+import ReactDOM from 'react-dom';
+// Require Editor CSS files.
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ProductCategory = [
+  { label: 'Food', value: 1 },
+  { label: 'Colthes', value: 2 },
+  { label: 'Commodity', value: 3 },
+  { label: 'Service', value: 4 },
+  { label: 'Others', value: 5 }
+];
 
 class PostForm extends Component {
   // static propTypes = {
@@ -35,84 +48,60 @@ class PostForm extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
-    //   Accounts.createUser(
-    //     {  profile: {
-    //       name: {
-    //         first: this.firstName.current.value,
-    //         last: this.lastName.current.value,
-    //       },
-    //       phoneNumber: this.phoneNumber.current.value,
-    //     },
-    //       address: this.address.current.value,
-    //       email: this.email.current.value,
-    //       title:this.title.current.value,
-    //       category:this.category.current.value,
-    //       content:this.content.current.value
-    //     },
-    //     (error) => {
-    //       if (error) {
-    //         console.log(error)
-    //         Bert.alert(error.reason, 'danger');
-    //       } else {
-    //         // Meteor.call('users.sendVerificationEmail');
-    //         Bert.alert('congratulations!', 'success');
-    //         this.props.history.push('/');
-    //       }
-    //     },
-    //   );
-    // }
-  }
+  };
 
   render() {
     return (
       <Form>
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+          <Form.Group as={Col} controlId='formGridFirstName'>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type='firstName' placeholder='Enter first name' />
           </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group as={Col} controlId='formGridLastName'>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type='lastName' placeholder='Enter Last name' />
           </Form.Group>
         </Form.Row>
-
-        <Form.Group controlId="formGridAddress1">
+        <Form.Group controlId='formGridPhoneNumber'>
+          <Form.Label>Phone</Form.Label>
+          <Form.Control type='phoneNumber' placeholder='PhoneNumber' />
+        </Form.Group>
+        <Form.Group controlId='formGridEmail'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+        </Form.Group>
+        <Form.Group controlId='formGridAddress1'>
           <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
+          <Form.Control placeholder='1234 Main St' />
         </Form.Group>
-        <Form.Group controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
-
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridCity">
+          <Form.Group as={Col} controlId='formGridCity'>
             <Form.Label>City</Form.Label>
-            <Form.Control />
+            <Form.Control placeholder='Vancouer' />
           </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control as="select">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
+          <Form.Group as={Col} controlId='formGridZip'>
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Control placeholder='XXXXXX' />
           </Form.Group>
         </Form.Row>
-
-        <Form.Group id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+        <Form.Group controlId='formGridProductName'>
+          <Form.Label>Title</Form.Label>
+          <Form.Control type='productTitle' placeholder='Product Name' />
         </Form.Group>
-
-        <Button variant="primary" type="submit">
+        <Form.Group controlId='formGridProductContent'>
+          <Form.Label>Product Description</Form.Label>
+          <FroalaEditorComponent tag='textarea' />
+        </Form.Group>
+        <Form.Group controlId='formGridProductCategory'>
+          <Form.Label>Product Category</Form.Label>
+          <Select options={ProductCategory} />
+        </Form.Group>
+        <Button>Add Photo</Button>
+        <Form.Group id='formGridCheckbox'>
+          <Form.Check type='checkbox' label='Agree with our rules' />
+        </Form.Group>
+        <Button type='submit' bsstyle='success'>
           Submit
         </Button>
       </Form>
