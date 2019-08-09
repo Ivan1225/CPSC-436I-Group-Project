ed00ce253c277bfc188a1a0e140c96d635a05eaa
Ivan Zhang <z243155476@gmail.com>
2019-08-08 22:24:08 -0700

    Package clean up

--- imports/ui/store/configure_store.js
+++ imports/ui/store/configure_store.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import combineActionsMiddleware from 'redux-combine-actions';
 import reducers from '../reducers';
 
 export default function configureStore(initialState) {
@@ -11,7 +10,6 @@ export default function configureStore(initialState) {
     initialState,
     compose(
       applyMiddleware(thunkMiddleware),
-      applyMiddleware(combineActionsMiddleware),
       process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     ),
   );
